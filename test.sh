38a0571aaf379e92b2b46947219fdc82ae1a80f4
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-07-18 02:00:23 -0700

    fix

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+source ./qstack.sh
+
+# Delete all branches except main
+git branch | grep -v "main" | xargs git branch -D 
+
+qstack create logging frontend
+qstack branch backend
+qstack push
+qstack create helm prometheus
+qstack list
+qstack switch logging
+qstack list-branches
+qstack rebase
