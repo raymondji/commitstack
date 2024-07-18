90a1fc312f0cf2163756421eeca86e40d1f1e7ea
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-07-17 23:14:00 -0700

    Try using periods

--- gitstack.sh
+++ gitstack.sh
@@ -13,11 +13,11 @@ gitstack-create() {
         return 1
     fi
 
-    NEW_BRANCH="$GS_BRANCH_PREFIX/$1/$2/$GS_TIP_OF_STACK"
+    NEW_BRANCH="$GS_BRANCH_PREFIX.$1.$2.$GS_TIP_OF_STACK"
     git checkout $GS_BASE_BRANCH && \
     git pull && \
     git checkout -b $NEW_BRANCH && \
-    git commit --allow-empty -m "$GS_BRANCH_PREFIX/$1/$2 start"
+    git commit --allow-empty -m "$GS_BRANCH_PREFIX.$1.$2 start"
 }
 
 gitstack-branch() {
@@ -31,32 +31,32 @@ gitstack-branch() {
         echo "You must be on the tip of the stack to add a new branch"
         return 1
     fi
-    RENAMED_CURRENT_BRANCH=${CURRENT_BRANCH%"/$GS_TIP_OF_STACK"}
-    STACK=$(echo $CURRENT_BRANCH | cut -d'/' -f2)
-    NEW_BRANCH="$GS_BRANCH_PREFIX/$STACK/$1/$GS_TIP_OF_STACK"
+    RENAMED_CURRENT_BRANCH=${CURRENT_BRANCH%".$GS_TIP_OF_STACK"}
+    STACK=$(echo $CURRENT_BRANCH | cut -d'.' -f2)
+    NEW_BRANCH="$GS_BRANCH_PREFIX.$STACK.$1.$GS_TIP_OF_STACK"
 
     git branch -m $RENAMED_CURRENT_BRANCH && \
     echo "Renamed branch $CURRENT_BRANCH -> $RENAMED_CURRENT_BRANCH" && \
     git checkout -b $NEW_BRANCH && \
-    git commit --allow-empty -m "$GS_BRANCH_PREFIX/$STACK/$1 start"
+    git commit --allow-empty -m "$GS_BRANCH_PREFIX.$STACK.$1 start"
 }
 
 gitstack-push() {
     CURRENT_BRANCH=$(git branch --show-current)
-    STACK=$(echo $CURRENT_BRANCH | cut -d'/' -f2)
+    STACK=$(echo $CURRENT_BRANCH | cut -d'.' -f2)
     if [ -z "$STACK" ]; then
         echo "Not within a stack"
         return 1
     fi
 
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}/$STACK/**/*")
+    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}.$STACK.**.*")
     if [ -z "$BRANCHES" ]; then
         echo "No branches found for stack '${1}'"
         return 1
     fi
     
     echo "$BRANCHES" | while IFS= read -r BRANCH; do
-        REMOTE_BRANCH=${BRANCH%"/$GS_TIP_OF_STACK"}
+        REMOTE_BRANCH=${BRANCH%".$GS_TIP_OF_STACK"}
         echo "Pushing branch $BRANCH -> $REMOTE_BRANCH"
         git push origin --set-upstream "$BRANCH":"$REMOTE_BRANCH" --force
         echo "" # newline
@@ -64,27 +64,27 @@ gitstack-push() {
 }
 
 gitstack-list() {
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}/**/*/$GS_TIP_OF_STACK")
+    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}.**.*.$GS_TIP_OF_STACK")
     if [ -z "$BRANCHES" ]; then
         echo "No stacks found"
         return 1
     fi
 
     echo "$BRANCHES" | while IFS= read -r BRANCH; do
-        STACK=$(echo $BRANCH | cut -d'/' -f2)
+        STACK=$(echo $BRANCH | cut -d'.' -f2)
         echo $STACK
     done
 }
 
 gitstack-list-branches() {
     CURRENT_BRANCH=$(git branch --show-current)
-    STACK=$(echo $CURRENT_BRANCH | cut -d'/' -f2)
+    STACK=$(echo $CURRENT_BRANCH | cut -d'.' -f2)
     if [ -z "$STACK" ]; then
         echo "Not within a stack"
         return 1
     fi
 
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/$GS_BRANCH_PREFIX/$STACK/**/*")
+    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/$GS_BRANCH_PREFIX.$STACK.**.*")
     if [ -z "$BRANCHES" ]; then
         echo "No branches found"
         return 1
@@ -101,7 +101,7 @@ gitstack-checkout() {
         return 1
     fi 
 
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}/${1}/*/$GS_TIP_OF_STACK")
+    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${GS_BRANCH_PREFIX}.${1}.*.$GS_TIP_OF_STACK")
     if [ -z "$BRANCHES" ]; then
         echo "No branches found for stack '${1}'"
         return 1
