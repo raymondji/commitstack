a65b1794dc3519a2734cd5de703493f61f3a3fc9
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-07-22 12:48:33 -0700

    raymondji/bar/1 start

--- qstack.sh
+++ qstack.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 QS_BASE_BRANCH=${QS_BASE_BRANCH:-main}
-QS_BRANCH_PREFIX=${QS_BRANCH_PREFIX:-$(whoami)}
-QS_TIP_OF_STACK="TIP"
 
 qs() {
     qstack "$@"
@@ -18,18 +16,12 @@ qstack() {
 
     if [ "$COMMAND" = "help" ] || [ "$COMMAND" = "h" ]; then
         qstack-help "$@"
-    elif [ "$COMMAND" = "create" ] || [ "$COMMAND" = "c" ]; then
-        qstack-create "$@"
-    elif [ "$COMMAND" = "branch" ] || [ "$COMMAND" = "b" ]; then
-        qstack-branch "$@"
     elif [ "$COMMAND" = "push" ] || [ "$COMMAND" = "p" ]; then
         qstack-push "$@"
-    elif [ "$COMMAND" = "list" ] || [ "$COMMAND" = "l" ]; then
+    elif [ "$COMMAND" = "list" ] || [ "$COMMAND" = "li" ]; then
         qstack-list "$@"
-    elif [ "$COMMAND" = "list-branches" ] || [ "$COMMAND" = "lb" ]; then
-        qstack-list-branches "$@"
-    elif [ "$COMMAND" = "switch" ] || [ "$COMMAND" = "s" ]; then
-        qstack-switch "$@"
+    elif [ "$COMMAND" = "log" ] || [ "$COMMAND" = "l" ]; then
+        qstack-log "$@"
     elif [ "$COMMAND" = "rebase" ] || [ "$COMMAND" = "r" ]; then
         qstack-rebase "$@"
     else
@@ -43,100 +35,31 @@ qstack-help() {
 
 subcommands:
 
-create ${stack} ${branch}
-    alias: c
-    create a new stack
-
-branch ${branch}
-    alias: b
-    create and checkout a new branch on top of the current stack
-
 push
     alias: p
     push all branches in the current stack to remote
 
-list
+log
     alias: l
+    git log helper
+
+list
+    alias: li
     list all stacks
 
-list-branches
-    alias: lb
-    lis all branches in the current stack (not ordered)
-
-switch ${stack}
-    alias: s
-    checkout the branch at the top of the stack
+branch
+    alias: b
+    list all branches in thestacks
 
 rebase
     alias: r
     start interactive rebase of the current stack against the base branch'
 }
 
-qstack-create() {
-    if [ -z "$1" ]; then
-        echo "Must specify new stack name"
-        return 1
-    fi
-    if [[ "$1" == *"/"* ]]; then
-        echo "Stack name cannot contain /"
-        return 1
-    fi
-    if [ -z "$2" ]; then
-        echo "Must specify a name for the first branch in the stack"
-        return 1
-    fi
-    if [[ "$2" == *"/"* ]]; then
-        echo "Branch name cannot contain /"
-        return 1
-    fi
-
-    NEW_BRANCH="$QS_BRANCH_PREFIX/$1/$2/$QS_TIP_OF_STACK"
-    git checkout $QS_BASE_BRANCH && \
-    git pull && \
-    git checkout -b $NEW_BRANCH && \
-    git commit --allow-empty -m "$QS_BRANCH_PREFIX/$1/$2 start"
-}
-
-qstack-branch() {
-    if [ -z "$1" ]; then
-        echo "Must specify new branch name"
-        return 1
-    fi
-    if [[ "$1" == *"/"* ]]; then
-        echo "Branch name cannot contain /"
-        return 1
-    fi
-
-    CURRENT_BRANCH=$(git branch --show-current)
-    STACK=$(parse-stack "$CURRENT_BRANCH")
-    if [ -z "$STACK" ]; then
-        echo "Not within a stack"
-        return 1
-    fi
-    if [[ ! "$CURRENT_BRANCH" == *"$QS_TIP_OF_STACK" ]]; then
-        echo "You must be on the tip of the stack to add a new branch"
-        return 1
-    fi
-    RENAMED_CURRENT_BRANCH=${CURRENT_BRANCH%"/$QS_TIP_OF_STACK"}
-    NEW_BRANCH="$QS_BRANCH_PREFIX/$STACK/$1/$QS_TIP_OF_STACK"
-
-    git branch -m $RENAMED_CURRENT_BRANCH && \
-    echo "Renamed branch $CURRENT_BRANCH -> $RENAMED_CURRENT_BRANCH" && \
-    git checkout -b $NEW_BRANCH && \
-    git commit --allow-empty -m "$QS_BRANCH_PREFIX/$STACK/$1 start"
-}
-
 qstack-push() {
-    CURRENT_BRANCH=$(git branch --show-current)
-    STACK=$(parse-stack "$CURRENT_BRANCH")
-    if [ -z "$STACK" ]; then
-        echo "Not within a stack"
-        return 1
-    fi
-
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${QS_BRANCH_PREFIX}/$STACK/**/*")
+    BRANCHES=$(git log --pretty='format:%D' main.. --decorate-refs=refs/heads)
     if [ -z "$BRANCHES" ]; then
-        echo "No branches found for stack '${1}'"
+        echo "No branches in the current stack"
         return 1
     fi
     
@@ -144,10 +67,8 @@ qstack-push() {
         EXISING_REMOTE_BRANCH=$(git for-each-ref --format='%(upstream:lstrip=3)' "refs/heads/$BRANCH")
         if [ -z "$EXISING_REMOTE_BRANCH" ]; then
             NEW_REMOTE_BRANCH=${BRANCH%"/$QS_TIP_OF_STACK"}
-            echo "Pushing to new remote branch $BRANCH -> $NEW_REMOTE_BRANCH"
             git push origin --set-upstream "$BRANCH":"$NEW_REMOTE_BRANCH" --force
         else
-            echo "Pushing to existing remote branch $BRANCH -> $EXISING_REMOTE_BRANCH"
             git push origin "$BRANCH":"$EXISING_REMOTE_BRANCH" --force
         fi
 
@@ -156,74 +77,30 @@ qstack-push() {
 }
 
 qstack-list() {
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${QS_BRANCH_PREFIX}/**/*/$QS_TIP_OF_STACK")
-    if [ -z "$BRANCHES" ]; then
-        echo "No stacks found"
-        return 1
-    fi
-
+    BRANCHES=$(git branch --format='%(refname:short)')
+    LEAVES=()
     echo "$BRANCHES" | while IFS= read -r BRANCH; do
-        STACK=$(parse-stack "$BRANCH")
-        echo $STACK
+        HAS_DESCENDENT=false
+
+        echo "$BRANCHES" | while IFS= read -r MAYBE_DESCENDENT; do
+            IS_ANCESTOR=$(git merge-base --is-ancestor $BRANCH $MAYBE_DESCENDENT; echo $?)
+            if [[ $BRANCH != $MAYBE_DESCENDENT ]] && [[ $IS_ANCESTOR == "0" ]]; then
+                HAS_DESCENDENT=true
+                break
+            fi
+        done
+
+        if [[ $HAS_DESCENDENT == false ]]; then
+            LEAVES+=($BRANCH)
+        fi
     done
+    git branch --list $LEAVES[@]
 }
 
-qstack-list-branches() {
-    CURRENT_BRANCH=$(git branch --show-current)
-    STACK=$(parse-stack "$CURRENT_BRANCH")
-    if [ -z "$STACK" ]; then
-        echo "Not within a stack"
-        return 1
-    fi
-
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/$QS_BRANCH_PREFIX/$STACK/**/*")
-    if [ -z "$BRANCHES" ]; then
-        echo "No branches found"
-        return 1
-    fi
-
-    echo "$BRANCHES" | while IFS= read -r BRANCH; do
-        echo $BRANCH
-    done
-}
-
-qstack-switch() {
-    if [ -z "$1" ]; then
-        echo "Must specify stack name"
-        return 1
-    fi 
-
-    BRANCHES=$(git for-each-ref --format='%(refname:short)' "refs/heads/${QS_BRANCH_PREFIX}/${1}/*/$QS_TIP_OF_STACK")
-    if [ -z "$BRANCHES" ]; then
-        echo "No branches found for stack '${1}'"
-        return 1
-    fi
-    
-    echo "$BRANCHES" | while IFS= read -r BRANCH; do
-        git checkout "$BRANCH"
-        return 0
-    done
+qstack-log() {
+    git log $QS_BASE_BRANCH.. --decorate-refs=refs/heads
 }
 
 qstack-rebase() {
-    CURRENT_BRANCH=$(git branch --show-current)
-    if [[ ! "$CURRENT_BRANCH" == *"$QS_TIP_OF_STACK" ]]; then
-        echo "You must be on the tip of the stack to rebase the stack"
-        return 1
-    fi
-
-    git checkout $QS_BASE_BRANCH && \
-    git pull && \
-    git checkout - && \
-    git rebase -i $QS_BASE_BRANCH --update-refs
-}
-
-parse-stack() {
-    # Check if there are at least 3 parts separated by '/'
-    if [[ $(echo "$1" | tr '/' '\n' | wc -l) -lt 3 ]]; then
-        echo ""
-    else
-        # Extract the second part (index 1, zero-based) which is 'stack'
-        echo "$1" | cut -d '/' -f 2
-    fi
+    git rebase -i $QS_BASE_BRANCH --update-refs --keep-base
 }
