d85489504add95c290bf2ea4969df42f37dc9562
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-10-11 06:14:35 -0700

    fix

--- test.sh
+++ test.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+set -e
+set -x
 
-echo "TODO"
+git checkout main
+git-stacked create a1
+git-stacked create a2
+git-stacked branch
+git checkout main
+git-stacked create b1
+git-stacked stack
+git checkout a2
+git-stacked push
