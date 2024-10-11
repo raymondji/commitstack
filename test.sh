782feba8c27f354e7e1ece45ddfe9eba76828697
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-10-11 06:59:29 -0700

    fix

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ debug() {
     echo ""
 }
 source ./stack.sh
-git update-index --assume-unchanged test-output.txt
+git update-index --assume-unchanged test-output-golden.txt
 GS_COLOR_OUTPUT=no
 
 # Run
@@ -25,9 +25,9 @@ echo "Running..."
     debug git-stacked stack
     debug git checkout a2
     debug git-stacked push-force
-) > test-output.txt 2>&1
+) > test-output-golden.txt 2>&1
 
 # Cleanup
 git checkout main
 git branch -D a1 a2 b1
-git update-index --no-assume-unchanged test-output.txt
+git update-index --no-assume-unchanged test-output-golden.txt
