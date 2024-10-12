71b82ac29f6a19221514cbf86f610368f413ee1f
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-10-12 15:49:03 -0700

    test

--- test.sh
+++ test.sh
@@ -16,25 +16,38 @@ TEST_GITHUB_REPO="git@github.com:raymondji/git-stacked-testing.git"
 echo "TEST_ROOTDIR: $TEST_ROOTDIR"
 
 run-test() {
-    VARIANT=$1
-    TEST_DIR="$TEST_ROOTDIR/$VARIANT"
+    set -x
+    TEST_VARIANT=$1
+    TEST_DIR="$TEST_ROOTDIR/$TEST_VARIANT"
     mkdir -p $TEST_DIR
     cd $TEST_DIR
 
     # Set up the git repo
-    if [ "$VARIANT" = "git" ] || [ "$VARIANT" = "github" ]; then
+    if [ "$TEST_VARIANT" = "git" ] || [ "$TEST_VARIANT" = "github" ]; then
         git clone $TEST_GITHUB_REPO .
-        rm -rf .git
+        REMOTE_URL=$(git config --get remote.origin.url)
+        if [ "$REMOTE_URL" != "$TEST_GITHUB_REPO" ]; then
+            # Sanity check before we do some of the subsequent destructive operations.
+            echo "Error: Remote origin is not set to X. It's set to $REMOTE_URL."
+            exit 1
+        fi
+
         git init
-        git remote add origin $TEST_GITHUB_REPO
+        for branch in $(git branch -r | grep -v '\->'); do
+            branch_name=$(echo $branch | sed 's/origin\///')
+            git push origin --delete $branch_name
+        done
+        git checkout --orphan main
+        git rm -rf .
         echo "This repo is used for testing git-stacked" > README.md
         git add .
-        git commit -am "First commit"
+        git commit -am "Reset repository to initial state"
         git push --force --set-upstream origin main
-    elif [ "VARIANT" = "gitlab" ]; then
+    elif [ "TEST_VARIANT" = "gitlab" ]; then
         echo "Gitlab test not implemented yet"
         return 1
     fi
+    set +x
 
     echo "Running..."
     (
@@ -48,7 +61,7 @@ run-test() {
 
         debug git checkout a2
         debug git-stacked push-force
-    ) > "$SOURCE_DIR/test-goldens/$VARIANT.txt" 2>&1
+    ) > "$SOURCE_DIR/test-goldens/$TEST_VARIANT.txt" 2>&1
 }
 
 # Run basic git test
