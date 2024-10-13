25adb037774443aa54dd0d1e51d7c4f634864a1d
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2024-10-12 21:05:41 -0700

    fix

--- stack.sh
+++ stack.sh
@@ -3,6 +3,7 @@ GS_BASE_BRANCH=${GS_BASE_BRANCH:-main}
 GS_ENABLE_COLOR_OUTPUT=${GS_ENABLE_COLOR_OUTPUT:-true} # false to disable
 GS_ENABLE_GITLAB_EXTENSION=${GS_ENABLE_GITLAB_EXTENSION:-false} # true to enable
 GS_ENABLE_GITHUB_EXTENSION=${GS_ENABLE_GITHUB_EXTENSION:-false} # true to enable
+GS_ENABLE_DEBUG_OUTPUT=${GS_ENABLE_DEBUG_OUTPUT:-false} # true to enable
 
 gs() {
     git-stacked "$@"
@@ -39,34 +40,28 @@ git-stacked() {
 
     if [ "$SUBCOMMAND" = "help" ] || [ "$SUBCOMMAND" = "h" ]; then
         git-stacked-help
-    elif [ "$SUBCOMMAND" = "push-force" ] || [ "$SUBCOMMAND" = "pf" ]; then
+    elif [ "$SUBCOMMAND" = "stack" ] || [ "$SUBCOMMAND" = "s" ]; then
+        git-stacked-stack "$@"
+    elif [ "$SUBCOMMAND" = "all" ] || [ "$SUBCOMMAND" = "a" ]; then
+        git-stacked-all
+    elif [ "$SUBCOMMAND" = "push" ]; then
         if [ $USE_EXTENSION = "gitlab" ]; then
-            gitlab-stacked-push-force
+            gitlab-stacked-push
         elif [ $USE_EXTENSION = "github" ]; then
-            github-stacked-push-force
+            github-stacked-push
         else
-            git-stacked-push-force
+            git-stacked-push
         fi
-    elif [ "$SUBCOMMAND" = "create" ] || [ "$SUBCOMMAND" = "c" ]; then
-        git-stacked-create "$@"
-    elif [ "$SUBCOMMAND" = "pull-rebase" ] || [ "$SUBCOMMAND" = "pr" ]; then
-        git-stacked-pull-rebase
-    elif [ "$SUBCOMMAND" = "rebase" ] || [ "$SUBCOMMAND" = "r" ]; then
-        git-stacked-rebase
+    elif [ "$SUBCOMMAND" = "pull" ]; then
+        git-stacked-pull
     elif [ "$SUBCOMMAND" = "branch" ] || [ "$SUBCOMMAND" = "b" ]; then
         git-stacked-branch
-    elif [ "$SUBCOMMAND" = "stack" ] || [ "$SUBCOMMAND" = "s" ]; then
-        git-stacked-stack
     elif [ "$SUBCOMMAND" = "log" ] || [ "$SUBCOMMAND" = "l" ]; then
         git-stacked-log
+    elif [ "$SUBCOMMAND" = "rebase" ] || [ "$SUBCOMMAND" = "r" ]; then
+        git-stacked-rebase
     elif [ "$SUBCOMMAND" = "reorder" ] || [ "$SUBCOMMAND" = "ro" ]; then
-        if [ $USE_EXTENSION = "gitlab" ]; then
-            gitlab-stacked-reorder
-        elif [ $USE_EXTENSION = "github" ]; then
-            github-stacked-reorder
-        else
-            git-stacked-reorder
-        fi
+        git-stacked-reorder
     else
         echo "Invalid subcommand"
         echo ""
@@ -80,16 +75,14 @@ git-stacked-help() {
 
 subcommands:
 
-create
-    alias: c
-    create a new branch on top of the current stack
+stack
+    alias: s
+    stack a new branch on top of the current stack
 
-push-force
-    alias: pf
+push
     push all branches in the current stack to remote
 
-pull-rebase
-    alias: pr
+pull
     update the base branch from mainstream, then rebase the current stack onto the base branch
 
 rebase
@@ -98,10 +91,10 @@ rebase
 
 log
     alias: l
-    git log helper
+    log commits in the current stack
 
-stack
-    alias: s
+all
+    alias: a
     list all stacks
 
 branch
@@ -113,51 +106,19 @@ reorder
     start interactive rebase to reorder branches in the current stack'
 }
 
-git-stacked-create() {
+git-stacked-stack() {
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    if ! is-top-of-stack && [ "$CURRENT_BRANCH" != "$GS_BASE_BRANCH" ]; then
+        echo "Can only be run from $GS_BASE_BRANCH or the top of a stack"
+        return 1
+    fi
+
     BRANCH=$1
     git checkout -b "$BRANCH"
     git commit --allow-empty -m "Start of $BRANCH"
 }
 
-git-stacked-branch() {
-    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-    BRANCHES=$(git log --pretty='format:%D' "$GS_BASE_BRANCH.." --decorate-refs=refs/heads | grep -v '^$')
-    if [ -z "$BRANCHES" ]; then
-        echo "Not in a stack"
-        return 1
-    fi
-
-    # is-top-of-stack returns 0 if the current branch is the tip of a stack
-    # otherwise 1
-    is-top-of-stack() {
-        DESCENDENT_COUNT=$(git branch --contains "$CURRENT_BRANCH" | wc -l)
-        if [[ "$DESCENDENT_COUNT" -eq 1 ]]; then
-            return 0
-        else
-            return 1
-        fi
-    }
-
-    while IFS= read -r BRANCH; do
-        if [ "$BRANCH" != "$CURRENT_BRANCH" ]; then
-            echo "  $BRANCH"
-        else
-            if [ "$GS_ENABLE_COLOR_OUTPUT" = "true" ]; then
-                printf "* \033[0;32m%s\033[0m" "$BRANCH" # green highlight
-            else
-                echo -n "* $BRANCH"
-            fi
-            
-            if is-top-of-stack; then
-                echo " (top)"
-            else
-                echo ""
-            fi
-        fi
-    done < <(echo "$BRANCHES")
-}
-
-git-stacked-stack() {
+git-stacked-all() {
     BRANCHES=$(git branch --format='%(refname:short)')
     STACKS=()
     while IFS= read -r BRANCH; do
@@ -192,11 +153,55 @@ git-stacked-stack() {
     done
 }
 
+git-stacked-branch() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    BRANCHES=$(git log --pretty='format:%D' "$GS_BASE_BRANCH.." --decorate-refs=refs/heads | grep -v '^$')
+    if [ -z "$BRANCHES" ]; then
+        echo "Not in a stack"
+        return 1
+    fi
+    if ! is-stack-valid; then
+        echo "Stack is invalid"
+        return 1
+    fi
+
+    while IFS= read -r BRANCH; do
+        if [ "$BRANCH" != "$CURRENT_BRANCH" ]; then
+            echo "  $BRANCH"
+        else
+            if [ "$GS_ENABLE_COLOR_OUTPUT" = "true" ]; then
+                printf "* \033[0;32m%s\033[0m (top)\n" "$BRANCH" # green highlight
+            else
+                echo "* $BRANCH"
+            fi
+        fi
+    done < <(echo "$BRANCHES")
+}
+
 git-stacked-log() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+
     git log "$GS_BASE_BRANCH.."
 }
 
-git-stacked-push-force() {
+git-stacked-push() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+    if ! is-stack-valid; then
+        echo "Stack is invalid"
+        return 1
+    fi
+
     # Reverse so we push from bottom -> top
     BRANCHES=$(git log --pretty='format:%D' "$GS_BASE_BRANCH.." --decorate-refs=refs/heads --reverse | grep -v '^$')
     if [ -z "$BRANCHES" ]; then
@@ -212,13 +217,22 @@ git-stacked-push-force() {
     done < <(echo "$BRANCHES")
 }
 
-gitlab-stacked-push-force() {
+gitlab-stacked-push() {
     echo "Gitlab extension not implemented yet, falling back to default behaviour."
     echo ""
     git-stacked-push-force
 }
 
-github-stacked-push-force() {
+github-stacked-push() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+    if ! is-stack-valid; then
+        echo "Stack is invalid"
+        return 1
+    fi
+
     # Reverse so we push from bottom -> top
     BRANCHES=$(git log --pretty='format:%D' "$GS_BASE_BRANCH.." --decorate-refs=refs/heads --reverse | grep -v '^$')
     if [ -z "$BRANCHES" ]; then
@@ -262,24 +276,38 @@ github-stacked-push-force() {
     done < <(echo "$BRANCHES")
 }
 
-git-stacked-pull-rebase() {
+git-stacked-pull() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+
     git checkout "$GS_BASE_BRANCH" && \
     git pull && \
     git checkout - && \
-    git rebase -i "$GS_BASE_BRANCH" --update-refs
+    git rebase "$GS_BASE_BRANCH" --update-refs
 }
 
 git-stacked-rebase() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+
     git rebase -i "$GS_BASE_BRANCH" --update-refs --keep-base
 }
 
 git-stacked-reorder() {
+    if ! is-top-of-stack; then
+        echo "Can only be run from the top of a stack"
+        return 1
+    fi
+
     echo "WARNING: this functionality needs work!"
     echo "Press enter to continue"
     read -r
     
     git checkout -b tmp-reorder-branch
-    gs create tmp-reorder-branch
     git rebase -i "$GS_BASE_BRANCH" --update-refs --keep-base
     BRANCHES=$(git log --pretty='format:%D' "$GS_BASE_BRANCH.." --decorate-refs=refs/heads | grep -v '^$')
     echo "After rebase branches: $BRANCHES"
@@ -289,14 +317,28 @@ git-stacked-reorder() {
     git branch -D tmp-reorder-branch
 }
 
-gitlab-stacked-reorder() {
-    echo "Gitlab extension not implemented yet, falling back to default behaviour."
-    echo ""
-    git-stacked-push-force
+# ========= HELPER FUNCTIONS ==========
+
+# is-top-of-stack returns 0 if all commits in the stack are valid
+# otherwise 1
+is-stack-valid() {
+    for COMMIT in $(git log --format="%H" "$GS_BASE_BRANCH.."); do
+        REF_COUNT=$(git show-ref --heads --tags | grep -c "$COMMIT")
+        if [ "$REF_COUNT" -gt 1 ]; then
+            echo "Error: commit $COMMIT has $REF_COUNT local refs pointing to it."
+            return 1
+        fi
+    done
 }
 
-github-stacked-reorder() {
-    echo "Github extension not implemented yet, falling back to default behaviour."
-    echo ""
-    git-stacked-push-force
+# is-top-of-stack returns 0 if the current branch is the tip of a stack
+# otherwise 1
+is-top-of-stack() {
+    CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+    DESCENDENT_COUNT=$(git branch --contains "$CURRENT_BRANCH" | wc -l)
+    if [[ "$DESCENDENT_COUNT" -eq 1 ]]; then
+        return 0
+    else
+        return 1
+    fi
 }
