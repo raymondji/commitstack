7b6dc20bd576116aa6857e23ea207c47d0720caf
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2025-01-04 14:50:25 -0800

    Fix

--- readme/generate.sh
+++ readme/generate.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if [[ -n "$(git status --porcelain)" ]]; then
-    echo "Error: Your working directory has uncommitted changes. Please commit or stash them before running this script."
-    exit 1
-fi
-
 SAMPLE_FILE=$(mktemp)
 go run ./cmd/git-stack learn --chapter 1 --mode=exec > "$SAMPLE_FILE"
 git checkout main
