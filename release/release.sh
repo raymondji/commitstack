8cd1e7390df9bd5c496bb254214668c655c670e7
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2025-01-04 14:36:48 -0800

    Fix codegen

--- release/release.sh
+++ release/release.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
 
-if [[ -n "$(git status --porcelain)" ]]; then
-  echo "Error: Your working directory has uncommitted changes. Please commit or stash them before running this script."
-  exit 1
-fi
+require_clean_repo() {
+  if [[ -n "$(git status --porcelain)" ]]; then
+    echo "Error: Your working directory has uncommitted changes. Please commit or stash them before running this script."
+    exit 1
+  fi
+}
 
+require_clean_repo
 go build ./...
 go test ./...
 go install ./cmd/git-stack
@@ -20,9 +23,8 @@ git stack learn --chapter 1 --mode=exec > "$SAMPLE_FILE"
 git checkout main
 git stack learn --chapter 1 --mode=clean
 
+# Ensure generated files are valid
 go run release/generate_template.go --template readme --sample-output "$SAMPLE_FILE"
-git add .
-git commit -m "Release $CLI_VERSION"
-git push
+require_clean_repo
 git tag "$CLI_VERSION"
 git push origin "$CLI_VERSION"
