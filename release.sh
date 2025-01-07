8c15027cfcc26b4eadcc376a489744489f0ec535
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2025-01-07 12:59:22 -0800

    Fix

--- release.sh
+++ release.sh
@@ -11,9 +11,6 @@ if git rev-parse "$CLI_VERSION" >/dev/null 2>&1; then
 fi
 
 readme/generate.sh
-git commit -am "Release $CLI_VERSION" --allow-empty
-git push
-
 git tag "$CLI_VERSION"
 git push origin "$CLI_VERSION"
 go install "github.com/raymondji/git-stack-cli/cmd/git-stack@$CLI_VERSION"
