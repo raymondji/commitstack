afd3270f641ad8f38c0a941450d8ebbe1436d355
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2025-01-06 22:07:57 -0800

    Fix

--- readme/generate.sh
+++ readme/generate.sh
@@ -4,14 +4,7 @@ set -e
 ORIGINAL_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 SAMPLE_FILE=$(mktemp)
 go install ./cmd/git-stack
-
-# git stack learn --mode=exec requires a clean git repo
-git add .
-git commit -m "Temp commit" --allow-empty 
-
 git stack learn --chapter 1 --mode=exec > "$SAMPLE_FILE"
 git checkout "$ORIGINAL_BRANCH"
 git stack learn --chapter 1 --mode=clean
 go run ./readme/generate.go --sample-output "$SAMPLE_FILE"
-
-git reset --hard HEAD^
