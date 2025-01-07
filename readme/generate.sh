89080d29c35ade1267e218fb1a9cf41644c7d01a
Raymond Ji <34181040+raymondji@users.noreply.github.com>
2025-01-06 21:52:58 -0800

    fix

--- readme/generate.sh
+++ readme/generate.sh
@@ -3,7 +3,9 @@ set -e
 
 go install ./cmd/git-stack
 SAMPLE_FILE=$(mktemp)
+git stash save # --mode=exec requires a clean git repo
 git stack learn --chapter 1 --mode=exec > "$SAMPLE_FILE"
 git checkout main
 git stack learn --chapter 1 --mode=clean
 go run ./readme/generate.go --sample-output "$SAMPLE_FILE"
+git stash pop
